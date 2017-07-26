8b0288518ba81d1038e9a6d9dc39a2910f5ca54a
jorge97710 <jazmitia_10@hotmail.com>
2017-07-26 07:22:24 -0600

    Se agregan metodos

--- Proveedores.java
+++ Proveedores.java
@@ -1,29 +1,31 @@
-
 public class Proveedores {
 	String nombre;
-	String numero;
-	String [] lista_juguetes;
-	
-	public Proveedores(String nombre, String numero, Object[] lista_juguetes) {
+	int numero;
+	String[] lista_juguetes;
+
+	public Proveedores(String nombre, int numero, Object[] lista_juguetes) {
 		this.nombre = nombre;
 		this.numero = numero;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getNumero() {
+
+	public int getNumero() {
 		return numero;
 	}
-	public void setNumero(String numero) {
+
+	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+
 	public Object[] getLista_juguetes() {
 		return lista_juguetes;
 	}
-
 
 }
